4f1db5b2dba0ebf4573d0a0d63738512d6062df3
Samuel Santana <35262710+turan-98@users.noreply.github.com>
2020-03-11 09:04:49 -0300

    Create lista1.sql

---
+++ LaboratorioDeDados/exercicios/lista1.sql
@@ -0,0 +1,43 @@
+Use Pessoa_Veiculo;
+-- exercicio 1 
+--SELECT 
+--	c.NomeCidade,u.nomeUF
+--FROM 
+--	Cidade c
+--INNER JOIN 
+--UF u on u.IdUF = c.IdUF
+
+--*---
+
+--exercicio 2
+-- SELECT 
+--	p.NomePessoa,c.NomeCidade as 'chá com pão'
+--FROM 
+	--Pessoa p
+--INNER JOIN 
+  -- Cidade c on c.IdCidade = p.CidadeReside
+
+  --*---
+
+--exercicio 3
+--SELECT 
+--	P.NomePessoa, c.idUf, c.NomeCidade
+--FROM
+-- Pessoa p
+--INNER JOIN
+--Cidade c on c.IdCidade = p.CidadeReside
+--*---
+
+--EXERCICIO 4---
+SELECT
+	p.NomePessoa, v.Placa, v.Ano, c.NomeCidade
+ From 
+	Pessoa p
+INNER JOIN
+	Veiculo v on v.IdPessoa = p.IdPessoa
+Inner join 
+	Cidade c on C.IdCidade = v.Cidade
+ORDER BY 1
+--*--
+
+
